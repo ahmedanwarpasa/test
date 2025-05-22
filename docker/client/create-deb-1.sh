ade8247399d76c7952056820f969c97fc38078db
ahmedanwarpasa <ahmed.anwar@ext.us.panasonic.com>
2025-05-22 11:24:01 +0300

    Add client stuff

---
+++ docker/client/create-deb-1.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+touch client-deb-dev_1.deb
+touch client-deb-core_1.deb
+
+echo $(date) > client-deb-core_1.deb
+echo $(date) > client-deb-dev_1.deb
---
+++ docker/client/create-deb-2.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+touch client-deb-dev_2.deb
+touch client-deb-core_2.deb
+
+echo $(date) > client-deb-core_2.deb
+echo $(date) > client-deb-dev_2.deb
